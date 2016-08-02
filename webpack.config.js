64e996c0a806e85f4542499bf18e46fc0ab5f952
yangwenliang <yang6233562@gmail.com>
2016-08-02 17:18:06 +0800

    将entry提成一个模块

--- webpack.config.js
+++ webpack.config.js
@@ -3,24 +3,29 @@
  * 适用单页面
  * 编译的目录结构与燕尾服相同
  *不一定非要全写成json，这个config就是一个node.js module，可以写任何JavaScript
- */ 
+ */
 
-var path=require('path');
+var path = require('path');
 var webpack = require('webpack');
 var glob = require('glob');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var feaEntry = require('fea-entry');
+var entry = feaEntry('dev/src/js/',['lib']);
+
+// console.log(entry);
+
 //抛出一个配置对象，供webpack使用
-var config ={
+var config = {
     //项目入口js
-    entry:{
-            index: "./dev/src/js/entry.js",
-            index1: "./dev/src/js/entry1.js"
-        },
+    entry: {
+        index: "./dev/src/js/entry.js",
+        index1: "./dev/src/js/entry1.js"
+    },
     //编译目录
-    output:{
+    output: {
         //文件编译的目标文件夹
-        path: path.join(__dirname,'build/static'),
+        path: path.join(__dirname, 'build/static'),
         //服务器的相对路径
         publicPath: "static/",
         filename: "js/[name].js",
@@ -30,7 +35,7 @@ var config ={
     },
     //对普通模块处理，就是我们自己写的那些模块
     module: {
-        loaders: [    //加载器
+        loaders: [ //加载器
             //babel编译jsx或者es6
             {
                 test: /\.jsx?$/,
@@ -39,22 +44,30 @@ var config ={
                 query: {
                     presets: ['es2015']
                 }
+            }, {
+                test: /\.html$/,
+                loader: "html"
             },
-            {test: /\.html$/, loader: "html" },
             //对css单独load，目的是可以编译到css目录下，否则会编译到js中当成模块，这样会白屏
-            {test: /\.css$/, loader:ExtractTextPlugin.extract("style", "css") },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract("style", "css")
+            },
             //小于4k变成base64
-            {test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192&name=./img/[hash].[ext]'}
+            {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader?limit=8192&name=./img/[hash].[ext]'
+            }
         ]
     },
-    plugins:[
-        new webpack.ProvidePlugin({    //加载jq
-                $: path.resolve(__dirname,'dev/src/js/lib/jquery'),
-                jQuery: path.resolve(__dirname,'dev/src/js/lib/jquery')
+    plugins: [
+        new webpack.ProvidePlugin({ //加载jq
+            $: path.resolve(__dirname, 'dev/src/js/lib/jquery'),
+            jQuery: path.resolve(__dirname, 'dev/src/js/lib/jquery')
                 // 或者直接用npm的jquery模块
                 // $: 'jquery'
-            }),
-        new ExtractTextPlugin("css/[name].css"),    //单独使用style标签加载css并设置其路径
+        }),
+        new ExtractTextPlugin("css/[name].css"), //单独使用style标签加载css并设置其路径
         // new HtmlWebpackPlugin({                        //根据模板插入css/js等标签生成最终HTML
         //     // favicon:'./src/img/favicon.ico', //favicon路径
         //     filename:'../index.html',    //生成的html存放路径，相对于 output.path
@@ -67,7 +80,10 @@ var config ={
         //         collapseWhitespace:false    //删除空白符与换行符
         //     }
         // }),
-        new webpack.optimize.CommonsChunkPlugin({name:'common',minChunks: 2})
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common',
+            minChunks: 2
+        })
         // new webpack.optimize.CommonsChunkPlugin({name:'doT',minChunks: Infinity}),
         // new webpack.optimize.CommonsChunkPlugin({name:'index'})
 
@@ -79,11 +95,11 @@ var config ={
         //这样就可以写 require('file') 代替 require('file.js')
         extensions: ['', '.js', '.json', '.scss'],
         alias: {
-            jquery: path.resolve(__dirname,'dev/src/js/lib/jquery')
+            jquery: path.resolve(__dirname, 'dev/src/js/lib/jquery')
         }
     },
     cache: true,
-    devtool:'source-map'
+    devtool: 'source-map'
 };
 
 var pages = Object.keys(getEntry('dev/*.html'));
@@ -91,19 +107,19 @@ pages.forEach(function(basename) {
     var conf = {
         filename: '../views/' + basename + '.html', //生成的html存放路径，相对于path
         template: 'dev/' + basename + '.html', //html模板路径
-        inject: true,    //js插入的位置，true/'head'/'body'/false
+        inject: true, //js插入的位置，true/'head'/'body'/false
         chunks: ['common'], //默认引用模块
         hash: true
-        /*
-        * 压缩这块，调用了html-minify，会导致压缩时候的很多html语法检查问题，
-        * 如在html标签属性上使用{{...}}表达式，所以很多情况下并不需要在此配置压缩项，
-        * 另外，UglifyJsPlugin会在压缩代码的时候连同html一起压缩。
-        * 为避免压缩html，需要在html-loader上配置'html?-minimize'，见loaders中html-loader的配置。
-         */
-        // minify: { //压缩HTML文件
-        //     removeComments: true, //移除HTML中的注释
-        //     collapseWhitespace: false //删除空白符与换行符
-        // }
+            /*
+             * 压缩这块，调用了html-minify，会导致压缩时候的很多html语法检查问题，
+             * 如在html标签属性上使用{{...}}表达式，所以很多情况下并不需要在此配置压缩项，
+             * 另外，UglifyJsPlugin会在压缩代码的时候连同html一起压缩。
+             * 为避免压缩html，需要在html-loader上配置'html?-minimize'，见loaders中html-loader的配置。
+             */
+            // minify: { //压缩HTML文件
+            //     removeComments: true, //移除HTML中的注释
+            //     collapseWhitespace: false //删除空白符与换行符
+            // }
     };
     if (basename in config.entry) {
         // conf.favicon = 'src/imgs/favicon.ico';
